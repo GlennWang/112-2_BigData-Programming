7aff9ab045d3b4b6272f74091bc7d2c91e1559a7
GlennWang <abc20200130abc@gmail.com>
2024-05-16 17:17:49 +0800

    week13

--- week13/p3.py
+++ week13/p3.py
@@ -20,13 +20,14 @@ print("每個六都區域的大專院校數目:")
 for city, count in uni_counts.items():
     print(f"{city}: {count}")
 
+max_city = max(uni_counts, key=uni_counts.get)
+
 uni_counts_df = pd.DataFrame(list(uni_counts.items()), columns=['縣市名稱', '大專院校數目'])
 
+explode = [0.2 if city == max_city else 0 for city in six_cities]
+
 fig, ax = plt.subplots(figsize=(8, 8))
-ax.pie(uni_counts.values(), labels=uni_counts.keys(), autopct='%1.1f%%', startangle=140, colors=plt.cm.Paired(range(len(uni_counts))))
+ax.pie(uni_counts.values(), explode=explode, labels=uni_counts.keys(), autopct='%1.1f%%', startangle=140, colors=plt.cm.Paired(range(len(uni_counts))))
 
 ax.set_title('六都區域內的大專院校數目佔比分析')
 plt.show()
-
-max_city = max(uni_counts, key=uni_counts.get)
-print(f"佔比最多的市: {max_city} ({uni_counts[max_city]} 所大專院校)")
