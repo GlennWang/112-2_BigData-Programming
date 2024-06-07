4250ad8923e79b67091e9cfdb14766d12e9d7fe2
Glenn Wang <abc20200130abc@gmail.com>
2024-06-08 03:32:30 +0800

    week16

--- week16/p1.py
+++ week16/p1.py
@@ -1,10 +1,8 @@
 import sqlite3
 
-# 連接到 SQLite 資料庫，資料庫不存在時會自動創建
 conn = sqlite3.connect('myDatabaseName.db')
 cursor = conn.cursor()
 
-# 創建 playerinfo 表
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS playerinfo (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,7 +14,6 @@ cursor.execute('''
     )
 ''')
 
-# 插入四筆資料
 cursor.execute('''
     INSERT INTO playerinfo (name, xpos, ypos, exp) 
     VALUES 
@@ -26,20 +23,17 @@ cursor.execute('''
     ('魏斯特', 73, 13, 5400)
 ''')
 
-# 確認資料插入
 cursor.execute('SELECT * FROM playerinfo')
 rows = cursor.fetchall()
 
 for row in rows:
     print(row)
 
-# 確認修改資料
 cursor.execute('SELECT name, exp FROM playerinfo')
 rows = cursor.fetchall()
 
 for row in rows:
     print(row)
 
-# 使用完畢後關閉連接
 conn.commit()
 conn.close()
--- week16/p4.py
+++ week16/p4.py
@@ -1,10 +1,7 @@
 import sqlite3
 import pandas as pd
 
-# Connect to the SQLite database
 conn = sqlite3.connect('students.db')
-
-# Define the query to count departments containing '資訊' for each school
 query = '''
 SELECT school, count(*) as department_count 
 FROM student112 
@@ -12,11 +9,8 @@ WHERE department LIKE '%資訊%'
 GROUP BY school;
 '''
 
-# Execute the query and load the data into a DataFrame
 df_info_departments = pd.read_sql_query(query, conn)
-
-# Close the database connection
 conn.close()
 
-# Display the result
+
 print(df_info_departments)
